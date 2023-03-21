50312c276abdbcef27e78d2225cc452f34f8bcf7
tonyaji19 <tonyajipalguno19@gmail.com>
2023-03-21 14:29:52 +0700

    COBA

--- config/Connect.js
+++ config/Connect.js
@@ -7,8 +7,9 @@ const db = new Sequelize(
   process.env.DB_DATABASE || "sequelize_db",
   process.env.DB_USER || "root",
   process.env.DB_PASSWORD || "",
+  process.env.PORT || 9000,
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
     dialectModule: mysql2,
   }
