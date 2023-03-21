b529d8789e49dbe65091d6a825dd27444c54e23b
tonyaji19 <tonyajipalguno19@gmail.com>
2023-03-21 14:31:20 +0700

    lagi

--- config/Connect.js
+++ config/Connect.js
@@ -7,7 +7,7 @@ const db = new Sequelize(
   process.env.DB_DATABASE || "sequelize_db",
   process.env.DB_USER || "root",
   process.env.DB_PASSWORD || "",
-  process.env.PORT || 9000,
+  process.env.PORT || "3306",
   {
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
