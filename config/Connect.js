eb5186d0d6da05dfd608fa92222f47ec8b2be49d
tonyaji19 <tonyajipalguno19@gmail.com>
2023-03-21 16:44:00 +0700

    cobalagis

--- config/Connect.js
+++ config/Connect.js
@@ -8,9 +8,34 @@ const db = new Sequelize(
   process.env.DB_USER || "root",
   process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST || "127.0.0.1",
-    dialect: "mysql",
-    dialectModule: mysql2,
+    // host: process.env.DB_HOST || "127.0.0.1",
+    // dialect: "mysql",
+    // dialectModule: mysql2,
+
+    development: {
+      username: "root",
+      password: null,
+      database: "sequelize_db",
+      host: "127.0.0.1",
+      dialect: "mysql",
+      dialectModule: mysql2,
+    },
+    test: {
+      username: "root",
+      password: null,
+      database: "sequelize_db",
+      host: "127.0.0.1",
+      dialect: "mysql",
+      dialectModule: mysql2,
+    },
+    production: {
+      username: "root",
+      password: null,
+      database: "sequelize_db",
+      host: "127.0.0.1",
+      dialect: "mysql",
+      dialectModule: mysql2,
+    },
   }
 );
 
